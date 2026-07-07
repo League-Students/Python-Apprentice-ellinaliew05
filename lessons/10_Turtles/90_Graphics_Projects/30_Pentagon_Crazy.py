f9433b8cf71d485b145c2a4769f7b5234cf35501
League Student <student@jointheleague.org>
2026-07-07 13:25:13 -0700

    Automated commit

--- lessons/10_Turtles/90_Graphics_Projects/30_Pentagon_Crazy.py
+++ lessons/10_Turtles/90_Graphics_Projects/30_Pentagon_Crazy.py
@@ -10,7 +10,7 @@ name: Pentagon Crazy
 import random
 import turtle
 
-colors = ("white", "deepskyblue", "crimson", "lightgreen", "lightblue")
+colors = ("white", "deepskyblue", "crimson", "mediumorchid", "lightblue")
 
 def get_random_color():
     return "#%06X" % (random.randint(0, 0xFFFFFF))
