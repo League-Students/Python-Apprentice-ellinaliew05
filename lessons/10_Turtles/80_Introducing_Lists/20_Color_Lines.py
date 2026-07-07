87674e4150ea7ef997246cf711787aa38161cb55
League Student <student@jointheleague.org>
2026-07-07 12:59:17 -0700

    Automated commit

--- lessons/10_Turtles/80_Introducing_Lists/20_Color_Lines.py
+++ lessons/10_Turtles/80_Introducing_Lists/20_Color_Lines.py
@@ -22,7 +22,7 @@ for color in colors:                            # loop through the colors
 # 2) Make another square, but put the colors in reverse order, using a negative index. 
 
 for i in range(4):
-    
+    colors[]
     tina.forward(50)
     tina.left(90)
 turtle.exitonclick()                     # Close the window when we click on it
