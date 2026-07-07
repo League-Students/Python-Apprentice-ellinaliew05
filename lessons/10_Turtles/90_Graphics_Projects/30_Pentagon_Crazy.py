d506ad0bc6281749ce50877cd47d8cf026ed385d
League Student <student@jointheleague.org>
2026-07-07 13:25:01 -0700

    Auto-commit: workspace changes before push

--- lessons/10_Turtles/90_Graphics_Projects/30_Pentagon_Crazy.py
+++ lessons/10_Turtles/90_Graphics_Projects/30_Pentagon_Crazy.py
@@ -10,7 +10,7 @@ name: Pentagon Crazy
 import random
 import turtle
 
-colors = ("indianred", "sandybrown", "palegoldenrod", "lightgreen", "lightblue")
+colors = ("white", "deepskyblue", "crimson", "lightgreen", "lightblue")
 
 def get_random_color():
     return "#%06X" % (random.randint(0, 0xFFFFFF))
