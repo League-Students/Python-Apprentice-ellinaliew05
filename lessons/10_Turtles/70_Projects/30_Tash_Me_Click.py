34c558687f29187a26e5d8dffd16e3aab4387be2
League Student <student@jointheleague.org>
2026-07-07 12:49:01 -0700

    Auto-commit: workspace changes before push

--- lessons/10_Turtles/70_Projects/30_Tash_Me_Click.py
+++ lessons/10_Turtles/70_Projects/30_Tash_Me_Click.py
@@ -5,7 +5,34 @@ Copy your old 20_Tash_Me.py code here and update the program to put the moustach
 
 Hint: See 10_More_Turtle_Programs, section 'Respond to Screen Clicks'
 """
- 
+ import turtle                                           # Import the turtle module
+
+screen = turtle.Screen()                                    # Set up the screen
+screen.setup(width=600, height=600)                         # Set the size of the window
+screen.bgcolor('white')                                     # Set the background color
+
+t = turtle.Turtle()                                         # Create a turtle
+t.shape("turtle")                                           # Set the shape of the turtle
+t.turtlesize(stretch_wid=10, stretch_len=10, outline=4)     # Make the turtle really big
+
+def turtle_clicked(t, x, y):
+    """Function that gets called when the user clicks on the turtle
+
+    This function will make the turtle tilt 20 degrees 18 times, making a full
+    circle. It is called by the turtle when the user clicks on it.
+
+    Args:
+        t (Turtle): The turtle object that was clicked
+        x (int): The x coordinate of the click
+        y (int): The y coordinate of the click
+    """
+
+    print('turtle clicked!')
+    
+
+
+# Connect the turtle to the turtle_clicked function
+t.onclick(lambda x, y, t=t: turtle_clicked(t, x, y))
 
 
 
