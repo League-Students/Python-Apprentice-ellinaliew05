84a845fd4ab1ee92caddfa342b524c10726bfe17
League Student <student@jointheleague.org>
2026-07-07 12:39:10 -0700

    Automated commit

--- lessons/10_Turtles/70_Projects/10_LeagueBot.py
+++ lessons/10_Turtles/70_Projects/10_LeagueBot.py
@@ -35,7 +35,7 @@ screen.setup(width=600, height=600)
 # Create a turtle and set its shape to the custom GIF
 t = turtle.Turtle()
 
-set_turtle_image(t, "pikachu.gif")
+set_turtle_image(t, "leaguebot_bot.gif")
 t.pencolor('blue')
 
 for i in range(6):
