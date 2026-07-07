410310f7cedffeb738778907b27c12fa1cb4ae5a
League Student <student@jointheleague.org>
2026-07-07 13:05:01 -0700

    Auto-commit: workspace changes before push

--- lessons/10_Turtles/90_Graphics_Projects/10_Flaming_Ninja_Star.py
+++ lessons/10_Turtles/90_Graphics_Projects/10_Flaming_Ninja_Star.py
@@ -34,12 +34,12 @@ t.shape("turtle")
 t.width(2)
 t.speed(0)
 
-for i in range(150):
+for i in range(80):
     t.pencolor(get_random_color())
     t.fillcolor(get_random_color())
     t.begin_fill()
-    t.forward(64)
-    t.left(10)
+    t.forward(80)
+    t.left(40)
     t.forward(flame_size)
     t.right(100)
     t.forward(flame_size)
