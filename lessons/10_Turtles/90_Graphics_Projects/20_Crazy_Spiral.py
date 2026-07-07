94257089711056c51ac9700c64c5efcd84709301
League Student <student@jointheleague.org>
2026-07-07 13:09:01 -0700

    Auto-commit: workspace changes before push

--- lessons/10_Turtles/90_Graphics_Projects/20_Crazy_Spiral.py
+++ lessons/10_Turtles/90_Graphics_Projects/20_Crazy_Spiral.py
@@ -8,25 +8,45 @@ uid: zfzMbyH7
 name: Crazy Spiral
 """
 
-... # Copy code to make a turtle and set up the window
+import random
+import turtle
 
-t = ... # Create a turtle named t
 
-# 1) Complete make_a_shape() to make the turtle move in some pattern. 
-# For instance, you can make it go left 30 degrees, then forward 50 pixels, 
-# then right 60 degrees, then forward 100 pixels. Make any shape you like.
+# Returns a random color!
+def get_random_color():
+    return "#%06X" % (random.randint(0, 0xFFFFFF))
 
-def make_a_shape(t):
-    """Make a shape with turtle t. Make it go left or right or forward"""    
-    ...
 
-# 2) Call make_a_shape() in a loop to make the turtle draw a spiral.
-# For instance, you can call make_a_shape() 100 times to make a spiral with 100 shapes.
-# The second ... in the for loop should be the number of shapes you want to make,
-# for example 100, or a list of numbers.
+colors = ["red", "blue", "green", "yellow", "orange"]
 
-num_shapes = ...
 
-for i in range(...):
-    make_a_shape(t)
-    t.right(360/num_shapes)
+def get_next_color(i):
+    return colors[i % len(colors)]
+
+turtle.setup(600, 600, 0, 0)            # Set the size of the window
+window = turtle.Screen()
+
+base_size = 200  # the size of the black part of the star
+flame_size = 130  # the length of the flaming arms
+
+t = turtle.Turtle()
+t.shape("turtle")
+t.width(2)
+t.speed(0)
+
+for i in range(80):
+    t.pencolor('')
+    t.fillcolor(get_random_color())
+    t.begin_fill()
+    t.forward(80)
+    t.left(10)
+    t.forward(flame_size)
+    t.right(170)
+    t.forward(flame_size)
+    t.right(100)
+    t.forward(base_size)
+    t.end_fill()
+
+t.hideturtle()
+
+turtle.done()
