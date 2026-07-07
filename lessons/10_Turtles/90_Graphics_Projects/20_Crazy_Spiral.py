7ebd5e3297c63332b878cdb61e7aa3cd98b266d1
League Student <student@jointheleague.org>
2026-07-07 13:16:16 -0700

    Automated commit

--- lessons/10_Turtles/90_Graphics_Projects/20_Crazy_Spiral.py
+++ lessons/10_Turtles/90_Graphics_Projects/20_Crazy_Spiral.py
@@ -79,7 +79,7 @@ for i in range(20):
     t.fillcolor('chartreuse')
     t.begin_fill()
     t.forward(30)
-    t.left(110)
+    t.left(120)
     t.forward(flame_size)
     t.right(170)
     t.forward(flame_size)
