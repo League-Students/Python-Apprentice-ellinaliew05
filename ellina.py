acd68a34029d468c3427ec99f581a73bc3961af4
League Student <student@jointheleague.org>
2026-07-09 13:20:01 -0700

    Auto-commit: workspace changes before push

--- ellina.py
+++ ellina.py
@@ -7,11 +7,6 @@ tina.speed (0)
 tina.shape('turtle')
 
 
-def fractal_triangle(size, depth):
-    fractal_triangle(size/2, depth-1)
-    tina.forward(size)
-    tina.left(120)
-
 def nudge(color):
     color += 1 + random.random() * 0.1 - 0.05
     return color % 1
@@ -21,4 +16,5 @@ def fractal_cool(size, depth, color):
     if depth == 0:
         tina.begin_fill()
         tina.color(color)
-        
+        for i in range(4):
+            
