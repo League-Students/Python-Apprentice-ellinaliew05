5a688b10c63b549b57a49fcecc4ec1b868f74eed
League Student <student@jointheleague.org>
2026-07-09 12:52:06 -0700

    Automated commit

--- ellina.py
+++ ellina.py
@@ -7,9 +7,9 @@ tina.speed(0)
 tina.shape('turtle')
 def fractal_octagon(size, depth): 
     if depth == 0:
-        for i in range(30):
+        for i in range(8):
             tina.forward(size)
-            tina.left(12)
+            tina.left()
     else:
         for i in range(30):
             fractal_triacontagon(size/2, depth-1)
