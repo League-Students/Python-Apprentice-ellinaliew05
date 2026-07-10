8c68cb8c8027266f0058ed07edf4276f9efa1dd9
League Student <student@jointheleague.org>
2026-07-10 12:43:06 -0700

    Automated commit

--- ellina.py
+++ ellina.py
@@ -10,7 +10,8 @@ cam_colors = ('red', 'black', 'white', 'blue', 'green')
 def open_cam_1():
     print("cam 1 open")
     screen.bgcolor(cam_colors[0])
-def open_cam_2
+def open_cam_2():
+    print()
 
 
 
