50a9365aab68fb31304c8576c3a1838cd8ddb19d
League Student <student@jointheleague.org>
2026-07-10 13:24:01 -0700

    Auto-commit: workspace changes before push

--- ellina.py
+++ ellina.py
@@ -29,28 +29,24 @@ def show_animatronics():
 def open_cam_1():
     global cam_num
     cam_num = 1
-    print("cam 1 open")
     screen.bgcolor(cam_colors[0])
     show_animatronics()
 
 def open_cam_2():
     global cam_num
     cam_num = 2
-    print("cam 2 open")
     screen.bgcolor(cam_colors[1])
     show_animatronics()
 
 def open_cam_3():
     global cam_num
     cam_num = 3
-    print("cam 3 open")
     screen.bgcolor(cam_colors[2])
     show_animatronics()
 
 def open_cam_4():
     global cam_num
     cam_num = 4
-    print("cam 4 open")
     screen.bgcolor(cam_colors[3])
     show_animatronics()
 
