9f35600edc244671f622930b359f22ffde01cc15
League Student <student@jointheleague.org>
2026-07-10 12:43:01 -0700

    Auto-commit: workspace changes before push

--- ellina.py
+++ ellina.py
@@ -1,36 +1,21 @@
 import turtle
-import random
 
 tina = turtle.Turtle()
+
 screen = turtle.Screen()
-screen.setup(600, 600)
-tina.speed (0)
-tina.shape('turtle')
+screen.setup(600,600)
+
+cam_colors = ('red', 'black', 'white', 'blue', 'green')
+
+def open_cam_1():
+    print("cam 1 open")
+    screen.bgcolor(cam_colors[0])
+def open_cam_2
 
 
-def nudge(color):
-    color += 1 + random.random() * 0.1 - 0.05
-    return color % 1
 
-def fractal_cool(size,depth,color):
-    tina.penup()
-    if depth == 0:
-        tina.begin_fill()
-        tina.color(color)
-        for i in range(4):
-            tina.forward(size)
-            tina.left(90)
-        tina.end_fill()
-    else:
-     for i in range(4):
-        color = (nudge(color[0]), nudge(color[1]), nudge(color[2]))
-        fractal_cool(size/2, depth-1,color)
-        tina.forward(size)
-        tina.left(90)
 
-tina.penup()
-tina.goto(-275, -275)
-tina.pendown()
-random_color = (0,0.5,0.05)
-fractal_cool(700, 6, random_color)
-turtle.exitonclick()
+screen.listen()
+screen.onkey(open_cam_1, "1")
+
+tina.exitonclick()
